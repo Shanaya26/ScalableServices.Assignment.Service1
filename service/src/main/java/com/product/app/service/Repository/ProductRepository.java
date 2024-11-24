704699325293405e75a0168d60012ed939c0d4d0
Sarthak Agrawal <sarthak.aggarwal1234@gmail.com>
2024-11-24 16:15:57 +0530

    Revert "Service 1 commit"
    This reverts commit b1b0ff4571d4e7b39415db0814265ec85ea08d6f.

--- service/src/main/java/com/product/app/service/Repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.product.app.service.Repository;
-
-import com.product.app.service.Models.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
